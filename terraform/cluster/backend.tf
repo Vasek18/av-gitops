75ec40cf81e90c295265df938b197122b60209dd
Vasiliy Aristov <aristovvasiliy92@gmail.com>
2026-08-04 23:27:14 +0300

    Move Terraform state to HCP Terraform

--- terraform/cluster/backend.tf
+++ terraform/cluster/backend.tf
@@ -1,21 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "av-gitops-tfstate"
-    key    = "cluster/terraform.tfstate"
-    region = "hel1"
+  cloud {
+    organization = "aristovvasiliy"
 
-    endpoints = {
-      s3 = "https://hel1.your-objectstorage.com"
+    workspaces {
+      name = "av-gitops-cluster"
     }
-
-    # Hetzner Object Storage is Ceph-based, not real AWS S3: these flags disable
-    # AWS-specific checks/behaviors that Ceph's S3-compatible API doesn't support.
-    skip_requesting_account_id  = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true
-    use_path_style              = true
   }
 }
-
